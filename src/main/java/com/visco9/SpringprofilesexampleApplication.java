eed58b371d9e952865d5332ab69410c68d783764
visco9 <visco9@gmail.com>
2017-02-11 15:20:02 -0500

    Initial commit

---
+++ src/main/java/com/visco9/SpringprofilesexampleApplication.java
@@ -0,0 +1,12 @@
+package com.visco9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprofilesexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprofilesexampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/visco9/SpringprofilesexampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.visco9;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringprofilesexampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
